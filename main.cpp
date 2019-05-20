8c665463644c77ba6497919d0135207cf86e0e01
prasanna kulal <32484352+prasannakulal16@users.noreply.github.com>
2019-05-20 20:06:48 +0530

    tetrahedron

--- main.cpp
+++ main.cpp
@@ -1,80 +1,85 @@
-
-
 #include<windows.h>
-#include <stdio.h>
-#include <GL/glut.h>
-#define PI 180
-#include<math.h>
-
-
-
-void expression(int x,int y,int r,int n,int l,int t)
+#include<stdlib.h>
+#include<stdio.h>
+#include<gl/glut.h>
+int m;
+float v[][3]={{0,0,1},{0,1,0},{-1,-0.5,0},{1,-0.5,0}};
+void init()
 {
 
-    float m;
-    glBegin(GL_LINE_STRIP);
+    glClearColor(0,0,0,0);
+    glOrtho(-1,1,-1,1,-1,1);
+    glEnable(GL_DEPTH_TEST);
+}
 
-     for( int angle = l; angle < t; angle ++ ){
-        m=angle*3.142/180;
-       glVertex2f( x+r*cos(m),y+n*sin(m));}
- glEnd();
+void drawtetra(float *a,float *b,float *c)
+{
 
+    glBegin(GL_TRIANGLES);
+    glVertex3fv(a);
+    glVertex3fv(b);
+    glVertex3fv(c);
+    glEnd();
+    glFlush();
+}
+
+void tetra(float *p,float *q,float *r ,float *s)
+{
+
+    glColor3f(1,0,0);
+    drawtetra(p,q,s);
+     glColor3f(0,1,0);
+    drawtetra(p,s,r);
+     glColor3f(0,0,1);
+    drawtetra(q,s,r);
+     glColor3f(0,1,1);
+    drawtetra(p,q,r);
+
+}
+
+void divide(float *x,float *y,float *z,float *h,int m)
+{
+
+    float p[6][6];
+    if(m>0)
+    {
+
+        for(int i=0;i<3;i++)
+        {
+
+            p[0][i]=(x[i]+z[i])/2;
+             p[1][i]=(y[i]+z[i])/2;
+              p[2][i]=(x[i]+y[i])/2;
+               p[3][i]=(x[i]+h[i])/2;
+                p[4][i]=(y[i]+h[i])/2;
+                 p[5][i]=(z[i]+h[i])/2;
+        }
+        divide(x,p[0],p[2],p[3],m-1);
+          divide(p[0],z,p[1],p[5],m-1);
+            divide(p[2],p[1],y,p[4],m-1);
+              divide(p[3],p[5],p[4],h,m-1);
+
+
+    }
+    else{
+        tetra(x,y,z,h);
+    }
 }
 void display()
 {
 
-    glClear(GL_COLOR_BUFFER_BIT);
-
-    glColor3f(0,0,0);
-  expression(280,300,23,50,0,360);
-glColor3f(0,0,0);
-
-   expression(330,300,23,50,0,360);
-
-    expression(300,300,210,250,166,376);
-
-    expression(150,470,120,120,8,245);
-
-     expression(450,470,120,120,-64,174);
-     glColor3f(1,0,0);
-     expression(300,489,30,5,-10,190);
-     glColor3f(0,1,0);
-     expression(300,180,55,35,0,360);
-      expression(300,190,130,80,180,360);
-      expression(170,170,30,20,50,130);
-         expression(430,170,30,20,50,130);
-         glColor3f(0,0,1);
-    expression(300,187,95,110,215,325);
-
-     expression(290,271,15,25,40,245);
-     expression(320,271,15,25,-58,140);
-
-     expression(237,290,90,150,45,200);
-    expression(365,290,91,150,-21,137);
-     expression(482,240,31,45,36,185);
-    glColor3f(1,0,1);
-        expression(120,240,31,45,10,155);
-
-
-glFlush();
-}
-
-void init(void)
-{
-	glClearColor(1,1,1,1);
-	gluOrtho2D(0,600,0,600);
-   glMatrixMode(GL_MODELVIEW);
-   glMatrixMode(GL_PROJECTION);
-   glLoadIdentity();
-
+    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+    divide(v[0],v[1],v[2],v[3],m);
 }
 int main()
 {
-    glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
-	glutInitWindowSize(600,600);
-	glutCreateWindow("gasket");
-	init();
-	glutDisplayFunc(display);
-	glutMainLoop();
-}
 
+    glutInitWindowSize(600,600);
+    glutInitDisplayMode(GLUT_SINGLE);
+    glutCreateWindow("gdfghd");
+    init();
+    printf("division\n");
+    scanf("%d",&m);
+    glutDisplayFunc(display);
+    glutMainLoop();
+}
