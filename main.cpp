38d205abc34236842def3489139d41593c9a6137
prasanna kulal <32484352+prasannakulal16@users.noreply.github.com>
2019-05-20 20:08:55 +0530

    tree

--- main.cpp
+++ main.cpp
@@ -1,52 +1,110 @@
 #include<windows.h>
-#include<GL/glut.h>
-#include<GL/glu.h>
-#include<GL/gl.h>
-#include<math.h>
-void init()
-{
+#include <stdio.h>
+#include <stdlib.h>
+#include <float.h>
+#include <math.h>
+#include <time.h>
+#include <iostream>
+using namespace std;
+#include <GL/glut.h>
 
-    glClearColor(0.0,0.0,0.0,1.0);
-	gluOrtho2D(0,500,0,500);
-	glMatrixMode(GL_PROJECTION);
-}
-void display()
-{
- int i; float x;
- glClear(GL_COLOR_BUFFER_BIT);
- glColor3f(1.0,1,0);
-  glBegin(GL_POINT);
- for(i=0;i<360;i++)
- {
-x=(i*3.142)/180;
- glVertex2f(250+150*cos(x),250+150*sin(x));
- }
+GLuint makeaTree;
+float x,y,z;
 
- /*for(i=240;i<300;i++)
- {
- glBegin(GL_POLYGON);
- x=cos((2*i*3.14)/360) * .3;
- y=sin((2*i*3.14)/360) * .3;
- glVertex2f(x,y);
- glEnd();
- }
- glPointSize(10.0);
- glBegin(GL_POLYGON);
- glVertex2f(-.25,.2);
- glVertex2f(.25,.2);*/
- glEnd();
-
- glFlush();
+void makeCylinder(float height, float base){
+GLUquadric *obj = gluNewQuadric();
+//gluQuadricDrawStyle(obj, GLU_LINE);
+glColor3f(0.64f, 0.16, 0.16f);glPushMatrix();
+glRotatef(-90, 1.0,0.0,0.0);
+gluCylinder(obj, base,base-(0.2*base), height, 20,20);
+glPopMatrix();
+glutSwapBuffers();
 }
 
-int main()
+
+
+
+void makeTree(float height, float base){
+
+float angle;
+makeCylinder(height, base); glTranslatef(0.0, height, 0.0);
+height -= height*.2; base-= base*0.3;
+for(int a= 0; a<3; a++){
+angle = rand()%50+20;
+if(angle >48)
+angle = -(rand()%50+20);
+if (height >1){
+glPushMatrix();
+glRotatef(angle,1,0.0,1);
+makeTree(height,base);
+glPopMatrix();
+
+}
+ //else glColor3f(0.0,1.0/a,0.0);glutSolidSphere(.2,10,10); add like fruits...
+}
+
+}
+void init(void)
+{ glClearColor(1.0,1.0,1.0,1.0);
+glShadeModel(GL_SMOOTH);
+glEnable(GL_DEPTH_TEST);
+makeaTree=glGenLists(1);
+glNewList(makeaTree, GL_COMPILE);
+makeTree(4,0.2);
+glEndList();
+}
+void keyboard(unsigned char key, int x, int y){
+switch (key){
+case 'x':
+x +=10;
+glutPostRedisplay();
+break;
+case 'y':
+y +=10;
+glutPostRedisplay();
+break;
+case 'z':
+z+=10;
+glutPostRedisplay();
+break;
+}
+}
+
+void display(){
+glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+glPushMatrix();
+glRotatef(x,1.0,0.0,0.0);
+glRotatef(y,0.0,1.0,0.0);
+glRotatef(z,0.0,0.0,1.0);
+glCallList(makeaTree);
+
+glPopMatrix();
+glutSwapBuffers();
+}
+void reshape(int w, int h)
+{ glViewport (0, 0, (GLsizei) w, (GLsizei) h);
+glMatrixMode(GL_PROJECTION);
+glLoadIdentity();
+gluPerspective(30.0, (GLfloat) w/(GLfloat) h, 0.001, 1000.0);
+glMatrixMode(GL_MODELVIEW);
+glLoadIdentity();
+glTranslatef(0.0,-8.0,-50.0);
+}
+int main(int argc, char **argv)
 {
- init();
- glutInitWindowSize(550,550);
- glutInitWindowPosition(0,0);
- glutInitDisplayMode(GLUT_RGB|GLUT_SINGLE);
- glutCreateWindow("simple");
- glutDisplayFunc(display);
+glutInit(&argc, argv);
+ glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+ glutInitWindowSize (1200, 800); glutInitWindowPosition(0,0);
+glutCreateWindow("3D Tree Using Recursion");
+init();
+glutReshapeFunc(reshape);
+ glutKeyboardFunc(keyboard);
+
+glutDisplayFunc(display);
+
+
  glutMainLoop();
- return 0;
+
+
 }
+
